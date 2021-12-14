671b4446a3b25468092257b4074f19ba906d4de6
The Grim Reaper <github.unemendable@slmail.me>
2021-12-14 22:02:19 +0500

    Boilerplate

--- Cafeteria Inventory Management System/src/Main/Item.java
+++ Cafeteria Inventory Management System/src/Main/Item.java
@@ -1,4 +1,69 @@
 package Main;
 
+import java.util.Date;
+
 public class Item {
+    private int itemID;
+    private String itemName;
+    private int itemQuantity;
+    private int itemReorderQuantity;
+    private Date itemExpiryDate;
+    private int itemPrice;
+
+    public Item(int itemID, String itemName, int itemQuantity, int itemReorderQuantity, Date itemExpiryDate, int itemPrice) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemReorderQuantity = itemReorderQuantity;
+        this.itemExpiryDate = itemExpiryDate;
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public int getItemReorderQuantity() {
+        return itemReorderQuantity;
+    }
+
+    public void setItemReorderQuantity(int itemReorderQuantity) {
+        this.itemReorderQuantity = itemReorderQuantity;
+    }
+
+    public Date getItemExpiryDate() {
+        return itemExpiryDate;
+    }
+
+    public void setItemExpiryDate(Date itemExpiryDate) {
+        this.itemExpiryDate = itemExpiryDate;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 }
--- Cafeteria Inventory Management System/src/Main/Recipe.java
+++ Cafeteria Inventory Management System/src/Main/Recipe.java
@@ -1,4 +1,47 @@
 package Main;
 
 public class Recipe {
+    private int recipeID;
+    private String recipeName;
+    private int recipePrice;
+    private int noOfIngredients;
+
+    public Recipe(int recipeID,String recipeName, int recipePrice, int noOfIngredients) {
+        this.recipeID = recipeID;
+        this.recipeName = recipeName;
+        this.recipePrice = recipePrice;
+        this.noOfIngredients = noOfIngredients;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public int getRecipePrice() {
+        return recipePrice;
+    }
+
+    public void setRecipePrice(int recipePrice) {
+        this.recipePrice = recipePrice;
+    }
+
+    public int getNoOfIngredients() {
+        return noOfIngredients;
+    }
+
+    public void setNoOfIngredients(int noOfIngredients) {
+        this.noOfIngredients = noOfIngredients;
+    }
 }
