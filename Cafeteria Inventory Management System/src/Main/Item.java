381deac88abe404b187d3fd3efa4f2ce7a2d7537
bubbles12356 <96119889+bubbles12356@users.noreply.github.com>
2021-12-14 12:00:28 +0000

    Update Item.java

--- Cafeteria Inventory Management System/src/Main/Item.java
+++ Cafeteria Inventory Management System/src/Main/Item.java
@@ -1,4 +1,67 @@
 package Main;
 
 public class Item {
+    private int itemID;
+    private String itemName;
+    private int itemQuantity;
+    private int itemReorderQuantity;
+    private String itemExpiryName;
+    private int itemPrice;
+
+    public Item(int itemID, String itemName, int itemQuantity, int itemReorderQuantity, String itemExpiryName, int itemPrice) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemReorderQuantity = itemReorderQuantity;
+        this.itemExpiryName = itemExpiryName;
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public int getItemReorderQuantity() {
+        return itemReorderQuantity;
+    }
+
+    public void setItemReorderQuantity(int itemReorderQuantity) {
+        this.itemReorderQuantity = itemReorderQuantity;
+    }
+
+    public String getItemExpiryName() {
+        return itemExpiryName;
+    }
+
+    public void setItemExpiryName(String itemExpiryName) {
+        this.itemExpiryName = itemExpiryName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 }
